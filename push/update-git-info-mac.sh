313c4e4061bca345f81c8ab5a015c7b7f54ec902
nightmar39 <anthonyzilla94@gmail.com>
2022-07-21 23:50:40 -0700

    testing one application without the namespace specified

--- push/update-git-info-mac.sh
+++ push/update-git-info-mac.sh
@@ -7,11 +7,11 @@ echo "Enter git repo name"
 
 read GIT_REPO
 
-for i in ../argocd-definitions/dev/app*; do 
-	sed -i '' -e "s/GITHUB_USER/$GIT_USER/" -e "s/GITHUB_REPO/$GIT_REPO/" -e "s/in-cluster/arn-aws-eks-us-east-2-835357571861-cluster-us-support-v2-clus/" $i
+for i in ./argocd-definitions/dev/app*; do 
+	sed -i '' -e "s/GITHUB_USER/$GIT_USER/" -e "s/GITHUB_REPO/$GIT_REPO/" -e "s/CLUSTER_NAME/arn-aws-eks-us-east-2-835357571861-cluster-us-support-v2-clus/" $i
 done 
 
-git add ../argocd-definitions/dev/app*
+git add ./argocd-definitions/dev/app*
 
 git branch -M main 
 
